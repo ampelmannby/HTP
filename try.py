4050f97888afa82af832fb819e1438e0129d1c86
kbz(work) <ampelmann.by@gmail.com>
2022-03-04 19:06:02 +0300

    Версия 2.0. Добавлен поиск по конкретной стране.

--- try.py
+++ try.py
@@ -1,37 +1,23 @@
+import time
 import traceback
 
 
-class Number:
-    a = int()
-    b = int()
+def decorator(func):
+    def inner(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except Exception:
+            e = traceback.format_exc(limit=0)
+            print(f'{e}{args}{kwargs}')
+
+    return inner
 
 
-num = 0
-
-
-def non_stop(func):
-
-    try:
-        func(num.a, num.b)
-    except Exception:
-        e = traceback.format_exc(limit=0)
-        print(f'{e}{num.a=}\n{num.b=}')
-
-
+@decorator
 def foo(a, b):
     c = a / b
     print(f'A / B = {c}')
 
 
-def start(a, b):
-    global num
-    num = Number()
-    num.a = a
-    num.b = b
-    @non_stop
-    def bar(a, b):
-        foo(num.a, num.b)
-    return
-
-start(4, 5)
+foo(4, 0)
 
